79e2dc68d3cb10f729a5d732bf9b75858ee7e5f0
Agmaromatic <Etu_AGMAR.AbdeslamYassine@emsi-edu.ma>
2021-11-11 22:16:09 +0100

    Search by type dossier exemple : http://localhost:8080/dossiers/search/findByTypeDossier?TypeDossier=Import  pour trouver les dossier imports

--- src/main/java/com/teranil/nejtrans/dao/DocumentRepository.java
+++ src/main/java/com/teranil/nejtrans/dao/DocumentRepository.java
@@ -4,6 +4,8 @@ import com.teranil.nejtrans.model.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
 @RepositoryRestResource
 public interface DocumentRepository extends JpaRepository<Document,Long> {
+
 }
--- src/main/java/com/teranil/nejtrans/dao/DossierRepository.java
+++ src/main/java/com/teranil/nejtrans/dao/DossierRepository.java
@@ -2,9 +2,12 @@ package com.teranil.nejtrans.dao;
 
 import com.teranil.nejtrans.model.Dossier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface DossierRepository extends JpaRepository<Dossier,Long> {
-
+List<Dossier> findByTypeDossier(String TypeDossier);
 }
