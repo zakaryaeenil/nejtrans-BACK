2b73af8245a4020fa61065dd783d4a02a8e7693e
Abdeslam Yassine Agmar <Etu_AGMAR.AbdeslamYassine@emsi-edu.ma>
2021-12-01 10:00:55 +0100

    user validation

--- src/main/java/com/teranil/nejtrans/model/User.java
+++ src/main/java/com/teranil/nejtrans/model/User.java
@@ -5,8 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -22,11 +26,22 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Length(max = 40, message = "First name is too long")
+    @NotEmpty(message = "First Name is required.")
+    @Column(name = "FIRST_NAME")
     private String firstName;
+    @Length(max = 40, message = "Last name is too long")
+    @NotEmpty(message = "Last name Name is required.")
+    @Column(name = "LAST_NAME")
     private String lastName;
+    @Column(nullable = false, unique = true)
     private String username;
     private String address;
+    @Column(nullable = false, unique = true)
     private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password should have a minimum of 6 characters")
+    @Column(nullable = false)
     private String password;
     @CreationTimestamp
     private Date createdAt;
