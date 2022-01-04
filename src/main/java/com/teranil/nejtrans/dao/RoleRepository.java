df33e0063372869f5d41c7c664c8094b9c83dae4
zakaryaeenil <79899217+zakaryaeenil@users.noreply.github.com>
2022-01-04 21:01:57 +0100

    Update RoleRepository.java

--- src/main/java/com/teranil/nejtrans/dao/RoleRepository.java
+++ src/main/java/com/teranil/nejtrans/dao/RoleRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://zakaryaeenil.github.io")
 @RepositoryRestResource
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
