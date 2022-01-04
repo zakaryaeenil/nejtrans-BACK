3b32b092e826d593485f413fe576ef718d3c5bd7
zakaryaeenil <79899217+zakaryaeenil@users.noreply.github.com>
2022-01-04 21:01:07 +0100

    Update DocumentRepository.java

--- src/main/java/com/teranil/nejtrans/dao/DocumentRepository.java
+++ src/main/java/com/teranil/nejtrans/dao/DocumentRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://zakaryaeenil.github.io")
 @RepositoryRestResource
 public interface DocumentRepository extends JpaRepository<Document, Long> {
 
